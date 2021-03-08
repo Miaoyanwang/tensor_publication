a65a207169c1421d37f3f65460fbd5df5e7ac477
Jiaxin Hu <marchhu@outlook.com>
2021-03-08 01:25:04 -0600

    revised matlab code

--- GLasso/rcode/brain_analysis/pipeline.m
+++ GLasso/rcode/brain_analysis/pipeline.m
@@ -4,18 +4,15 @@
 % Here is the pipeline to use the Joint_tucker_v6.m function.
 % Remember to revise the paths!
 
-
-userpath(func_path); % REVISED!
+func_path = % REVISED!
+userpath(func_path); 
 % func_path should be the path involves the main function and the logdet.m
 
 % Read ss data 
-load("ss_data.mat");
+load("ss_data.mat");% REVISED!
 
 % read n_vector(sample size vector)
 gnn = readtable("nvector.csv");% REVISED!
-% nvec_path should be the path of the sample size vector
-% nvec_path should be different with ss_data_path, 
-% unless sample size vector is not stored as .csv
 gnn = gnn{:,:};
 gnn(:,1) = [];
 gnn = gnn';
@@ -36,13 +33,15 @@ r = 3; option.rho = 1000;
 
 % result_path should be the path you store the results
 
+result_path = % REVISED!
+
 for i = 1:r % if the rank degenerates, change r to the degenerated rank.
-    csvwrite("result_path/Theta_"+i+"_r"+r+"_rho"+option.rho+".csv",Theta{i});% REVISED!
+    csvwrite(result_path+"Theta_"+i+"_r"+r+"_rho"+option.rho+".csv",Theta{i});% REVISED!
 end
 
-csvwrite("result_path/U"+"_r"+r+"_rho"+option.rho+".csv",U);% REVISED!
-csvwrite("result_path/Theta0"+"_r"+r+"_rho"+option.rho+".csv",Theta0);% REVISED!
-csvwrite("result_path/Obj_curve"+"_r"+r+"_rho"+option.rho+"_iter" + option.Niter+".csv",rec_obj);% REVISED!
+csvwrite(result_path+"U"+"_r"+r+"_rho"+option.rho+".csv",U);% REVISED!
+csvwrite(result_path+"Theta0"+"_r"+r+"_rho"+option.rho+".csv",Theta0);% REVISED!
+csvwrite(result_path+"Obj_curve"+"_r"+r+"_rho"+option.rho+"_iter" + option.Niter+".csv",rec_obj);% REVISED!
 
 
 
