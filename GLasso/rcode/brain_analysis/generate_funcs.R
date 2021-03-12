2553c85ec32766ed6a8cf8a4668d2f322fcebd34
Jiaxin Hu <marchhu@outlook.com>
2021-03-11 23:14:08 -0600

    update

--- GLasso/rcode/brain_analysis/generate_funcs.R
+++ GLasso/rcode/brain_analysis/generate_funcs.R
@@ -6,25 +6,43 @@ library(rmatio)
 library(plot.matrix)
 library(RColorBrewer)
 
-
 generate_ss_data = function(exp_data, select_gene, path){
-  # exp_data should be a list of gene expression data
+  # exp_data should be a list of gene expression data with tissue name
   # selec_genes is the index for the selected genes
   # path is where we store the .mat file
   
+  # covert gene name
+  cut_exp = exp_data[[1]][select_gene,]
+  geneid = rownames(cut_exp)
+  geneid = gsub("\\..*","",geneid)
+  
+  covert = ensembldb::select(EnsDb.Hsapiens.v79, keys= geneid, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+  
+  # some gene can not be encoded
+  index = geneid %in% covert$GENEID
+  gene_name = geneid
+  gene_name[index] = covert$SYMBOL
+  
+  filename = paste0(path,"/gene_name.csv")
+  write.csv(gene_name,filename)
+
   ss_list = list()
   for (i in 1:13) {
     cut_exp = exp_data[[i]][select_gene,]
+    
+    rownames(cut_exp) = gene_name
     ss_list[[i]] = cov(t(cut_exp))
   }
   
-  # write ss_list as a single .mat file
-  output_list = list()
-  output_list[[1]] = ss_list # then matlab recognize the mat as a single cell
-  names(output_list) = "ss_data"
+  names(ss_list) = names(exp_data)
   
-  filename = paste0(path,"/ss_data.mat")
-  write.mat(output_list,filename)
+  # write ss_list as a single .mat file
+  output_list = list(ss_data = ss_list, gene_name = gene_name)
+  #output_list[[1]] = ss_list # then matlab recognize the mat as a single cell
+  #names(output_list) = "ss_data"
+  
+  filename = paste0(path,"/ss_data.mat" )
+  write.mat(output_list,filename, compression = F)
 }
 
 generate_ss_figure = function(ss_data,path){
@@ -41,3 +59,35 @@ generate_ss_figure = function(ss_data,path){
   }
   
 }
+
+
+## 03/11/21 update
+
+# Code to generate varvar data
+# load("/Users/March/Desktop/myresearch/graphical_lasso/data_analysis/gene_full/corrected_expression_list_noY.RData")
+# brain_tissue_index = c(14,33,39:49)
+# 
+# gtex_brain = list()
+# for (i in 1:13) {
+#   gtex_brain[[i]] = corrected_expression_list[[brain_tissue_index[i]]]
+# }
+# brain_tissue_name = colnames(sample[1,,]) 
+# brain_tissue_name_sub = gsub(" - ", "",brain_tissue_name)
+# brain_tissue_name_sub = gsub(" ", "", brain_tissue_name_sub)
+# names(gtex_brain) = brain_tissue_name_sub
+# 
+# write.csv(brain_tissue_name,"/Users/March/Desktop/myresearch/graphical_lasso/data_analysis/multi_layer/test1/varvar_withname/tissue_name.csv")
+
+# a = c("Brain - Cortex")
+# gsub(" - ", "",a)
+# 
+# brain_gene_tissue_var = matrix(0,ncol = 13,nrow = dim(corrected_expression_list[[1]])[1])
+# for (i in 1:13) {
+#   t_exp = gtex_brain[[i]]
+#   brain_gene_tissue_var[,i] = apply(t_exp, 1, var)
+# }
+# 
+# brain_gene_var_var = apply(brain_gene_tissue_var,1,var)
+# brain_top_var_var_index = order(brain_gene_var_var,decreasing = T)[1:300]
+# 
+# generate_ss_data(gtex_brain,brain_top_var_var_index,"/Users/March/Desktop/myresearch/graphical_lasso/data_analysis/multi_layer/test1/varvar_withname")
