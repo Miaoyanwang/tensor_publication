910132044dfddb142767b568a991b44ae95e1bcc
miaoyan wang <saplingwang@gmail.com>
2021-10-04 23:03:31 -0500

    update

--- tensor_clustering/code/sample.R
+++ tensor_clustering/code/sample.R
@@ -5,16 +5,18 @@ source("functions.R")
 source("HOLloyd.R")
 source("tensor_score.R")
 
-
+set.seed(1)
 
 ############## CER vs gamma
 p <- 100
 r <- 5
 
 #gamma_range <- c(-2.1, -2, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4)
-gamma_range <- c(-2.1, -1.9, -1.7, -1.5, -1.4)
+##gamma_range <- c(-2.1, -1.9, -1.7, -1.5, -1.4)
+gamma_range <- c(-1.5,-1.4,-1.3,-1.2,-1.1)
 
-alpha_range = 1 + 180*p^(gamma_range/2)
+
+alpha_range = 1 + 100*p^(gamma_range/2)
 
 s_min <- 0.5
 delta = 0.5
@@ -34,9 +36,9 @@ for (g in 1:length(gamma_range)) {
   s_max <- s_min * alpha_range[g]
   # our_err <- c()
   our_err2 <- c()
-  # han_err <- c()
-  # hosvd_err1 <- c()
-  # hosvd_err2 <- c()
+  #han_err <- c()
+  #hosvd_err1 <- c()
+  #hosvd_err2 <- c()
   ke_err <- c()
   # ke_err2 <- c()
   # ke_err3 <- c()
@@ -65,17 +67,17 @@ for (g in 1:length(gamma_range)) {
     our_err2 <- c(our_err2, CER(re_result, dat$z))
     
     # # han
-    # z.HOSC = HO.SC(as.tensor(dat$Y), r) # high-order initialization
+    #  z.HOSC = HO.SC(as.tensor(dat$Y), r) # high-order initialization
     # z.Lloyd.HOSC = HO.Lloyd(as.tensor(dat$Y), z.HOSC) # HLloyd iteration
     # han_err = c(han_err,min(CER(z.Lloyd.HOSC[[1]], dat$z) ,CER(z.Lloyd.HOSC[[2]], dat$z) ,CER(z.Lloyd.HOSC[[3]], dat$z) ))
     # 
     # # hosvd1
-    # hosvd1 = HOSVD_kmeans(dat$Y, r)
+    #hosvd1 = HOSVD_kmeans(dat$Y, r)
     # hosvd_err1 = c(hosvd_err1, CER(hosvd1, dat$z))
     # 
     # # hosvd2
-    # hosvd2 = HOSVD_score(dat$Y, r)
-    # hosvd_err2 = c(hosvd_err2, CER(hosvd2, dat$z))
+    #  hosvd2 = HOSVD_score(dat$Y, r)
+    #hosvd_err2 = c(hosvd_err2, CER(hosvd2, dat$z))
     
     # tensor score
     ke_result =  tensor_score_adj(dat$Y, r, rm_diag = T, hooi = T, 
@@ -107,14 +109,31 @@ for (g in 1:length(gamma_range)) {
   #mis[1, g] <- mean(our_err)
   mis[2, g] <- mean(our_err2)
   # mis[3, g] <- mean(han_err)
-  # mis[4, g] <- mean(hosvd_err1)
-  # mis[5, g] <- mean(hosvd_err2)
+  #mis[4, g] <- mean(hosvd_err1)
+  #mis[5, g] <- mean(hosvd_err2)
   mis[6, g] <- mean(ke_err)
   # mis[7, g] <- mean(ke_err2)
   # mis[8, g] <- mean(ke_err3)
   # mis[9, g] <- mean(ke_err4)
   #mis[10, g] <- mean(ke_err5)
 }
+library(ggplot2)
+data=mis[2:6,]
+rownames(data)=c("ours","han","hosvd","hosvd+","Ke")
+
+require(reshape2)
+dataL=melt(data)
+dataL[,2]=rep(gamma_range,rep(5,5))
+p  <- ggplot(dataL, aes_string(x="Var2", y="value", colour="Var1", group="Var1"))
+p <- p + geom_line()+geom_point()
+
+plot(gamma_range,mis[2,],type="l",ylim=c(0,0.25))
+points(gamma_range,mis[3,],type="l",col="red")
+points(gamma_range,mis[4,],type="l",col="blue")
+points(gamma_range,mis[5,],type="l",col="green")
+points(gamma_range,mis[6,],type="l",col="purple")
+dev.copy(pdf,"compare.pdf")
+
 
 
 
