4cf50eb3460807528d76bc7650679250bd6abd92
Jiaxin Hu <marchhu@outlook.com>
2022-01-20 14:29:18 -0600

    Update Table3.R

--- tensor_clustering/code/Table3.R
+++ tensor_clustering/code/Table3.R
@@ -5,5 +5,10 @@ install.packages("software/dTBM.tar.gz", repos = NULL, type = "source")
 library(dTBM)
 library(RSKC)
 
+true_z = factor(peru$attr_data$party)
+levels(true_z) = 1:5
+
 # start analysis ------------------------------------------------------------------------------------------------
+
 result = dtbm(peru$network_data, r = rep(5,3), max_iter = 20, asymm = F)
+CER(result$z[[1]], true_z)
